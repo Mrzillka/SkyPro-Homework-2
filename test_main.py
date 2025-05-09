d5f4bc6deb91904128108a08e14d0db09b409808
Mrzillka <mrzillka@gmail.com>
2025-05-09 23:00:58 +0700

    Added task 1: raise ValueError on creating product with zero quantity

--- test_main.py
+++ test_main.py
@@ -218,3 +218,7 @@ def test_category_iterator(category):
     assert lst == ["Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.",
                    "Iphone 15, 210000.0 руб. Остаток: 8 шт.",
                    "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт."]
+
+def test_product_zero_quantity():
+    with pytest.raises(ValueError):
+        Product("...", "...", "...", 0)
